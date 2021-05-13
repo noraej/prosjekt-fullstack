43a4e1bd87a14d03a7994e82e08130181833ca5d
Nora Evensen Jansrud <noraej@live.no>
2021-05-13 16:09:37 +0200

    initial commit

---
+++ frontend/src/views/LogIn.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class = "logIn">
+        <span>
+            <button @click="logInAsUser">User log in</button>
+            <button @click="logInAsAdmin">Admin log in</button>
+        </span>
+        <div id="userLogIn" v-if="scene === 1">
+            <h3>Email</h3>
+            <input type="text" placeholder="email">
+            <h3>Password</h3>
+            <input type="password">
+            <h4 id="feedback"></h4>
+            
+        </div>
+        <div id="adminLogIn" v-if="scene === 2">
+            <h3>Email</h3>
+            <input type="text" placeholder="email">
+            <h3>Password</h3>
+            <input type="password">
+            <h4 id="feedback"></h4>
+        </div>
+        <button @click="logIn">Log in</button>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+        const scene = ref(1);
+        const logInAsAdmin = ref(() => {
+            scene.value = 2;
+        });
+        const logInAsUser = ref(() => {
+            scene.value = 1;
+        });
+        const logIn = ref(() => {
+            //method for logging inn
+        });
+        
+        return{
+            scene,
+            logInAsAdmin, 
+            logInAsUser,
+        }
+    },
+})
+</script>
