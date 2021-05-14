86299fdd7c740b79564d2217f2eb71571a626a21
idaht <ida.trosdahl@gmail.com>
2021-05-14 15:00:29 +0200

    Half finished reservationDTOs, missing some stuff

--- backend/src/main/java/idatt2105/hamsterGroup/fullstackProject/Model/Reservation.java
+++ backend/src/main/java/idatt2105/hamsterGroup/fullstackProject/Model/Reservation.java
@@ -1,7 +1,8 @@
 package idatt2105.hamsterGroup.fullstackProject.Model;
 
 import javax.persistence.*;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
 
 /**
  * Entity class for storing information about
@@ -13,7 +14,18 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long reservationId;
     private int numberOfUsers;
-    private LocalDate reservationDateTime;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String description;
+    private int duration;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roomId")
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buildingId")
+    private Building building;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId")
@@ -23,12 +35,26 @@ public class Reservation {
     @JoinColumn(name = "sectionId")
     private Section section;
 
-    public Reservation(long reservationId, int numberOfUsers, LocalDate reservationDateTime, User user, Section section) {
+    @OneToOne
+    @JoinColumn(name = "sectionId", referencedColumnName = "sectionId")
+    private Set<Section> sections;
+
+    public Reservation(long reservationId, int numberOfUsers, LocalDateTime startTime, LocalDateTime endTime, String description,
+                       User user, Section section, int duration, Room room, Building building, Set<Section> sections) {
         this.reservationId = reservationId;
         this.numberOfUsers = numberOfUsers;
-        this.reservationDateTime = reservationDateTime;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
         this.user = user;
-        this.section = section;
+        this.duration = duration;
+        this.room = room;
+        this.building = building;
+        if (section != null) {
+            this.section = section;
+        } else {
+            this.sections = sections;
+        }
     }
 
     public long getReservationId() {
@@ -47,12 +73,36 @@ public class Reservation {
         this.numberOfUsers = numberOfUsers;
     }
 
-    public LocalDate getReservationDateTime() {
-        return reservationDateTime;
+    public LocalDateTime getStartTime() {
+        return startTime;
     }
 
-    public void setReservationDateTime(LocalDate reservationDateTime) {
-        this.reservationDateTime = reservationDateTime;
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
     }
 
     public User getUser() {
@@ -71,14 +121,42 @@ public class Reservation {
         this.section = section;
     }
 
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
+
+    public Set<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(Set<Section> sections) {
+        this.sections = sections;
+    }
+
     @Override
     public String toString() {
         return "Reservation{" +
-                "reservationId=" + reservationId +
-                ", numberOfUsers=" + numberOfUsers +
-                ", reservationDateTime=" + reservationDateTime +
-                ", user=" + user +
-                ", section=" + section +
+                "\nreservationId=" + reservationId +
+                ", \nnumberOfUsers=" + numberOfUsers +
+                ", \nstartTime=" + startTime +
+                ", \nendTime=" + endTime +
+                ", \ndescription=" + description +
+                ", \nduration=" + duration +
+                ", \nuser=" + user +
+                ", \nsection=" + section +
+                ", \nbuiding=" + building +
                 '}';
     }
 }
