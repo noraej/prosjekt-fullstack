69423624be51af9e9a3f8034a37c4b02f332a9fd
Linda Larsen <Linda_larsen@live.no>
2021-05-20 11:58:38 +0200

    Update CreateUser.interface.ts

--- frontend/src/interfaces/CreateUser.interface.ts
+++ frontend/src/interfaces/CreateUser.interface.ts
@@ -1,12 +1,11 @@
 import LogInUser from "./LoginUser.interface";
 
-export default interface SignUpUser extends LogInUser {
+export default interface CreateUser extends LogInUser {
   firstname: string;
   lastname: string;
   email: string;
   phoneNumber: string;
   valid: boolean;
   admin: boolean;
-  role: string;
 
 }
