f2db1973f0b284f9bdb88ff62dcaa57360719ed7
Nora Evensen Jansrud <noraej@live.no>
2021-05-20 13:20:14 +0200

    made my bookings view

--- frontend/src/views/UserBookings.vue
+++ frontend/src/views/UserBookings.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <p>YO</p>
+    <UserHeader />
+    <h2 id="title">Bookings</h2>
+    <div></div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import UserHeader from "../components/UserHeader.vue";
 
 export default defineComponent({
   name: "UserBookings",
+  components: {
+    UserHeader,
+  },
   setup() {
     const bookings = ref();
     return {
